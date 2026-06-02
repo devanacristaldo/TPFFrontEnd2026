5a1d164425b2e9931e73b216615e39bcabb65a22
Ana <dev.anacristaldo@gmail.com>
2026-06-02 19:49:47 -0300

    Datepicker added

--- TPFinal/src/Home.jsx
+++ TPFinal/src/Home.jsx
@@ -3,19 +3,30 @@ import { useState, useEffect } from "react";
 
 function Home() {
 
-    const [data, setData] = useState(null);
+const [fecha, setFecha] = useState("");
 
-    useEffect(() => {
-        fetch("https://api.agify.io/?name=Ana")
-            .then((response) => response.json())
-            .then((data) => setData(data));
-    }, []);
-
-    if (data !== null) {
         return (
             <>
                 <h1>TP Final Dev-01</h1>
                 <div>
+                     <div className="container mt-5">
+      <h2>Fecha</h2>
+      
+      <div className="mb-3">
+        <label htmlFor="fecha" className="form-label">
+          Selecciona una fecha:
+        </label>
+        <input
+          type="date"
+          id="fecha"
+          className="form-control"
+          value={fecha}
+          onChange={(e) => setFecha(e.target.value)}
+        />
+      </div>
+
+      <p>Fecha seleccionada: {fecha}</p>
+    </div>
 
 
                     <div class="field">
@@ -92,6 +103,6 @@ function Home() {
         )
     }
 
-}
+
 
 export default Home 
--- TPFinal/src/main.jsx
+++ TPFinal/src/main.jsx
@@ -5,5 +5,8 @@ import App from './App.jsx'
 import Home from './Home.jsx'
 
 createRoot(document.getElementById('root')).render(
+  <>
     <Home />
+    
+     </>
 )
